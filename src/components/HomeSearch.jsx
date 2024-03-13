6a93c5280be257f500957ae8f9dd543a7e29e06b
akash-panthri <akashpanthri2000@gmail.com>
2024-03-14 04:12:58 +0530

    Added buttons to HomeSearch Component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -2,10 +2,17 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 export default function HomeSearch() {
   return (
+    <>
+        
     <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
         <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
         <input type="text" className="flex-grow focus:outline-none" />
         <BsFillMicFill className="text-lg" />
     </form>
+    <div>
+        <button className="btn">Google Search</button>
+        <button className="btn">I am Feeling Lucky</button>
+    </div>
+    </>
   )
 }
