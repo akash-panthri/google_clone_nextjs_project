5381eee056ae657548336776abc2160049ab9546
akash-panthri <akashpanthri2000@gmail.com>
2024-03-14 04:17:59 +0530

    Styled buttons container in  HomeSearch Component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -9,7 +9,7 @@ export default function HomeSearch() {
         <input type="text" className="flex-grow focus:outline-none" />
         <BsFillMicFill className="text-lg" />
     </form>
-    <div>
+    <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
         <button className="btn">Google Search</button>
         <button className="btn">I am Feeling Lucky</button>
     </div>
