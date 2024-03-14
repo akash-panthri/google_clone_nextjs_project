cdc89837a2e46b1306a81da52245dc45607a8620
akash-panthri <akashpanthri2000@gmail.com>
2024-03-15 04:03:29 +0530

    Added search functionality in HomeSearch Component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,18 +1,33 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 export default function HomeSearch() {
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  }
+ 
   return (
     <>
-        
-    <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+      <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
         <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
-        <input type="text" className="flex-grow focus:outline-none" />
+        <input
+          type="text"
+          className="flex-grow focus:outline-none"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
         <BsFillMicFill className="text-lg" />
-    </form>
-    <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
-        <button className="btn">Google Search</button>
+      </form>
+      <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+        <button onClick={handleSubmit} className="btn">Google Search</button>
         <button className="btn">I am Feeling Lucky</button>
-    </div>
+      </div>
     </>
-  )
+  );
 }
