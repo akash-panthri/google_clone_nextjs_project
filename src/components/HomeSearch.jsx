2715d79154001c841aa6a5c8fec69ef5c26ce688
akash-panthri <akashpanthri2000@gmail.com>
2024-03-15 04:15:31 +0530

    Added loading state to randome search and corrected styling in HomeSearch Component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -5,6 +5,8 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 export default function HomeSearch() {
   const [input, setInput] = useState("");
+  
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
   const router = useRouter();
   function handleSubmit(e) {
     e.preventDefault();
@@ -12,11 +14,13 @@ export default function HomeSearch() {
     router.push(`/search/web?searchTerm=${input}`);
   }
   async function randomSearch() {
+    setRandomSearchLoading(true);
     const response = await fetch("https://random-word-api.herokuapp.com/word")
       .then((res) => res.json())
       .then((data) => data[0]);
     if (!response) return;
     router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
   }
   return (
     <>
@@ -33,12 +37,20 @@ export default function HomeSearch() {
         />
         <BsFillMicFill className="text-lg" />
       </form>
-      <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8 ">
         <button onClick={handleSubmit} className="btn">
           Google Search
         </button>
-        <button onClick={randomSearch} className="btn">
-          I am Feeling Lucky
+        <button onClick={randomSearch} className="btn flex items-center justify-center disabled:opacity-80">
+        {randomSearchLoading ? (
+            <img
+              src="spinner.svg"
+              alt="loading..."
+              className="h-6 text-center"
+            />
+          ) : (
+            "I am Feeling Lucky"
+          )}
         </button>
       </div>
     </>
