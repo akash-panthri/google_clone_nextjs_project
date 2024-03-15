992849aa6e221c1ae5db704644346662145eb578
akash-panthri <akashpanthri2000@gmail.com>
2024-03-16 04:07:32 +0530

    Added CountryLookup Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import CountryLookup from "./CountryLookup";
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
-  )
+    <footer>
+      <div className="">
+        <CountryLookup />
+      </div>
+      <div className="">
+        <ul>
+          <li>About</li>
+          <li>Advertising</li>
+          <li>Business</li>
+          <li>How Search works</li>
+        </ul>
+
+        <ul>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Settings</li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
