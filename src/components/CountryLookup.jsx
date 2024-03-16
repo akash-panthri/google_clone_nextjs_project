45b2685ecd1e305d492586214f78aeb3e49fc879
akash-panthri <akashpanthri2000@gmail.com>
2024-03-17 04:03:34 +0530

    Made CountryLookup Component functional

--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const url = `https://freeipapi.com/api/json`; 
+
 
 export default function CountryLookup() {
-  return (
-    <div>CountryLookup</div>
-  )
+  const [country, setCountry] = useState("United States");
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data;
+        setCountry(data.countryName);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return <div>{country}</div>;
 }
