fa3e365e095ff2d889a8f41c583629b7d17e75e7
akash-panthri <akashpanthri2000@gmail.com>
2024-03-19 04:03:21 +0530

    Modified SearchHeader Component

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
 
 export default function SearchHeader() {
   return (
-    <div>SearchHeader</div>
-  )
+    <header>
+      <div className="">
+        <Link href={"/"}>
+          <Image
+            width="120"
+            height="40"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+            alt="Google"
+          />
+        </Link>
+
+        <div className="">
+          <RiSettings3Line/>
+          <TbGridDots/>
+        </div>
+        <button>
+          Sign in
+        </button>
+      </div>
+    </header>
+  );
 }
