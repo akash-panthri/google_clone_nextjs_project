7c4e2646fa17c4b26ecd7da7832cf8b7a95b2b56
akash-panthri <akashpanthri2000@gmail.com>
2024-03-20 04:18:50 +0530

    Styled SearchBox Component

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,11 +5,11 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchBox() {
   return (
-    <form>
-    <input type="text" />
-    <RxCross2 />
-    <BsFillMicFill />
-    <AiOutlineSearch />
+    <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center" >
+    <input type="text" className="w-full focus:outline-none" />
+    <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" />
+    <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3"  />
+    <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" />
     
     </form>
   )
