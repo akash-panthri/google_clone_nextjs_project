e7f834abec478d7a4bf0291466a04a643c881eeb
akash-panthri <akashpanthri2000@gmail.com>
2024-03-20 04:10:11 +0530

    Modified SearchBox Component

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { RxCross2 } from "react-icons/rx"
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchBox() {
   return (
-    <div>SearchBox</div>
+    <form>
+    <input type="text" />
+    <RxCross2 />
+    <BsFillMicFill />
+    <AiOutlineSearch />
+    
+    </form>
   )
 }
