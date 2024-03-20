b0bf250e5990946e0e16c102af38689c7c548153
akash-panthri <akashpanthri2000@gmail.com>
2024-03-21 04:35:54 +0530

    Modified SearchHeaderOptions Component

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,6 +1,27 @@
+"use client"
+
+
+import { AiOutlineCamera, AiOutlineSearch } from "react-icons/ai";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchHeaderOptions() {
+    const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
+  function selectTab(tab) {
+    router.push(`/search/${tab === "Images" ? "image" : "web"}?searchTerm=${searchTerm}`);
+  }
   return (
-    <div>SearchHeaderOptions</div>
+    <div>
+    <div onClick={()=>selectTab("All")} >
+        <AiOutlineSearch />
+        <p>All</p>
+      </div>
+      <div onClick={()=>selectTab("Images")} >
+        <AiOutlineCamera />
+        <p>Images</p>
+      </div>
+    </div>
   )
 }
