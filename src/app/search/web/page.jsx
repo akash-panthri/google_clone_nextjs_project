30b26ae7f98ec8aae46ec145f28c50b62974df87
akash-panthri <akashpanthri2000@gmail.com>
2024-03-22 04:02:23 +0530

    Added google api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default async function WebSearchPage() {
+
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=joggle`
+  );
   return (
     <div>WebSearchPage</div>
   )
