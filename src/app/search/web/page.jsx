e124fc5fc6bd88b8c519f6722a31d66322b039e3
akash-panthri <akashpanthri2000@gmail.com>
2024-03-22 04:13:00 +0530

    Added searchParams

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default async function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
 
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=joggle`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
   const data = await response.json();
