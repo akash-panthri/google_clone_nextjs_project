af116c94d85ef3cc6ded91e4af0936f92e4cb304
akash-panthri <akashpanthri2000@gmail.com>
2024-03-23 04:29:31 +0530

    Modified error.jsx in search directory

--- src/app/search/error.jsx
+++ src/app/search/error.jsx
@@ -1,8 +1,14 @@
-"use client"
+"use client";
 
 
-export default function Error() {
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+    useEffect(() => {
+        console.log("Error: ", error);
+      }, [error]);
   return (
-    <div>error</div>
-  )
+    <div className="flex flex-col justify-center items-center pt-10"> <h1 className="text-3xl mb-4">Something went wrong</h1>
+    <button className="text-blue-500" onClick={() => reset()}>Try again</button></div>
+  );
 }
