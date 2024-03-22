b5adc5dd1d49936e66c20335909111bc0ae12b42
akash-panthri <akashpanthri2000@gmail.com>
2024-03-23 04:03:01 +0530

    Handled error for google api result

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -5,7 +5,10 @@ export default async function WebSearchPage({searchParams}) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
-
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("Something went wrong");
+  }
   const data = await response.json();
 
   const results = data.items;
