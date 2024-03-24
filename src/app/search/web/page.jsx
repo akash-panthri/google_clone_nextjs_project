a4f6e38a8750513af4a1ccbce4d039a320b3353a
akash-panthri <akashpanthri2000@gmail.com>
2024-03-24 08:12:39 +0530

    Styled error for 'no results found'

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -13,13 +13,13 @@ export default async function WebSearchPage({searchParams}) {
   const data = await response.json();
 
   const results = data.items;
-  if (!results) {
+if (!results) {
     return (
-      <div >
-        <h1 >No results found</h1>
-        <p >
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
           Try searching for something else or go back to the homepage{" "}
-          <Link href="/">
+          <Link href="/" className="text-blue-500">
             Home
           </Link>
         </p>
