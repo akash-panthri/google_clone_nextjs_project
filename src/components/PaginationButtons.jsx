d3db91f3a5488b2a7df7791cf3ecfe3004f1a9c0
akash-panthri <akashpanthri2000@gmail.com>
2024-04-07 07:32:23 +0530

    Created previous page Link in PaginationButtons

--- src/components/PaginationButtons.jsx
+++ src/components/PaginationButtons.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-
+import Link from 'next/link';
 import { usePathname, useSearchParams } from "next/navigation";
 export default function PaginationButtons() {
     const pathname = usePathname();
@@ -8,6 +8,14 @@ export default function PaginationButtons() {
   const searchTerm = searchParams.get("searchTerm");
   const startIndex = +searchParams.get("start") || 1;
   return (
-    <div>PaginationButtons</div>
+    <div>{startIndex >= 10 && (
+        <Link
+          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
+        >
+          <div className="">
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}</div>
   )
 }
