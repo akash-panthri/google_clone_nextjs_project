9e0dd599a50c6c0afd8ff266d3cc49cfb098a8c6
akash-panthri <akashpanthri2000@gmail.com>
2024-04-07 07:25:51 +0530

    Made  PaginationButtons client side

--- src/components/PaginationButtons.jsx
+++ src/components/PaginationButtons.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+"use client";
 
+
+import { usePathname, useSearchParams } from "next/navigation";
 export default function PaginationButtons() {
+    const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
+  const startIndex = +searchParams.get("start") || 1;
   return (
     <div>PaginationButtons</div>
   )
