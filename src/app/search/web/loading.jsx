bacb302b916bf1a64eddb0f5ebb7296e51d582ff
akash-panthri <akashpanthri2000@gmail.com>
2024-04-09 04:11:24 +0530

    Added and styled html in  loading page to web folder

--- src/app/search/web/loading.jsx
+++ src/app/search/web/loading.jsx
@@ -1,6 +1,13 @@
 
 export default function loading() {
   return (
-    <div>loading</div>
+    <>
+    <div>
+        <div className="h-2.5 w-48 bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-3.5 max-w-[360px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-2 max-w-[560px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-2 max-w-[530px] bg-gray-200 rounded-full mb-2.5"></div>
+    </div>
+</>
   )
 }
