c0c44155b68644298ebcfa4aac30218633d8a4d7
akash-panthri <akashpanthri2000@gmail.com>
2024-04-10 04:15:01 +0530

    Added and styled html in  loading page in image folder

--- src/app/search/image/loading.jsx
+++ src/app/search/image/loading.jsx
@@ -1,6 +1,12 @@
 
 export default function loading() {
   return (
-    <div>loading</div>
+    <div>
+    <div className="animate-pulse">
+        <div className="h-48 w-48 mb-4 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-48 mb-2.5 bg-gray-200 rounded-md"></div>
+        <div className="h-2 w-44 mb-2.5 bg-gray-200 rounded-md"></div>
+      </div>
+    </div>
   )
 }
